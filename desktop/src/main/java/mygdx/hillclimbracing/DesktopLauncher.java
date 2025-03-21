634b6e58ce9a0ecbd493858b904654c4ed833b61
DAEMONSUKUNA <158506511+DAEMONSUKUNA@users.noreply.github.com>
2025-03-21 12:19:31 +0530

    Update DesktopLauncher.java

--- desktop/src/main/java/mygdx/hillclimbracing/DesktopLauncher.java
+++ desktop/src/main/java/mygdx/hillclimbracing/DesktopLauncher.java
@@ -6,10 +6,10 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 public class DesktopLauncher {
     public static void main(String[] arg) {
         Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-        config.setTitle("Hill Climb Racing"); // Set window title
-        config.setFullscreenMode(Lwjgl3ApplicationConfiguration.getDisplayMode()); // Set fullscreen
-        config.useVsync(true); // Enable vsync
-        config.setForegroundFPS(60); // Limit FPS to 60
+        config.setTitle("Hill Climb Racing"); 
+        config.setFullscreenMode(Lwjgl3ApplicationConfiguration.getDisplayMode()); 
+        config.useVsync(true); 
+        config.setForegroundFPS(60);
         new Lwjgl3Application(new HillClimbGame(), config);
     }
 }
